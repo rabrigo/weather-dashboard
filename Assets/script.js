5f635863576fbda4af82353ee135ae256fe8eae5
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-01-09 18:25:16 -0800

    updated cards

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,9 @@
-var testOne = document.createElement('h1');
-testOne.textContent = "Hello";
-console.log(testOne);
-document.body.appendChild(testOne);
-var request = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=28f599eb217e2b8510f6add9afd17c7b";
+var cityDate = document.querySelector('.city-date');
+var tempDisplay = document.querySelector('.temp-display');
+var windDisplay = document.querySelector('.wind-display');
+var humidityDisplay = document.querySelector('.humidity-display');
+var uvIndexDisplay = document.querySelector('.uv-index-display');
+var request = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=28f599eb217e2b8510f6add9afd17c7b&units=imperial";
 
 fetch(request)
         .then(function (response) {
@@ -10,7 +11,12 @@ fetch(request)
         })
         .then(function (data) {
                 console.log(data);
-                testOne.textContent = data.timezone;
+                // console.log(data.weather[0].main);
+                cityDate.textContent = `${data.name} (${moment().format('MM/DD/YYYY')}): ${data.main.temp}\u00B0F`;
+                tempDisplay.textContent = `${data.main.temp}\u00B0F`;
+                windDisplay.textContent = `${data.wind.speed} MPH`;
+                humidityDisplay.textContent = `${data.main.humidity}`;
+                uvIndexDisplay.textContent = `${data.main.humidity}`;
               });
             
 
